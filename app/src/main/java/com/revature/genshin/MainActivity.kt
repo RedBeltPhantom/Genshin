5da3b9604a730aa816785fe2953ed8bb9bae0eae
mwoods19 <mathew.woods@revature.net>
2021-08-26 12:41:05 -0400

    Initial commit

---
+++ app/src/main/java/com/revature/genshin/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.revature.genshin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.revature.genshin.ui.theme.GenshinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GenshinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GenshinTheme {
+        Greeting("Android")
+    }
+}
